8918fd4b25590c45c0dae8941ae071b02bba7e2f
sanjay kumar <sanjayconnecting007@gmail.com>
2026-05-14 14:11:01 +0530

    Add 301 redirects for old venue URLs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import TeakMeadows from './pages/TeakMeadows';
 import Muddainapalya from './pages/Muddainapalya';
@@ -14,6 +14,18 @@ function App() {
       <Route path="/muddainapalya" element={<Muddainapalya />} />
       <Route path="/ullal" element={<Ullal />} />
       <Route path="/rrnagar" element={<RRNagar />} />
+
+      {/* Redirects for old URLs */}
+      <Route path="/muddainapalya-asthitva-the-venue" element={<Navigate replace to="/muddainapalya" />} />
+      <Route path="/muddainapalya-asthitva-the-venue/*" element={<Navigate replace to="/muddainapalya" />} />
+      <Route path="/asthitva-terrace-garden" element={<Navigate replace to="/ullal" />} />
+      <Route path="/asthitva-terrace-garden/*" element={<Navigate replace to="/ullal" />} />
+      <Route path="/rr-nagar-asthitva-the-venue" element={<Navigate replace to="/rrnagar" />} />
+      <Route path="/rr-nagar-asthitva-the-venue/*" element={<Navigate replace to="/rrnagar" />} />
+      <Route path="/asthitva-teak-meadows" element={<Navigate replace to="/magadi-road" />} />
+      <Route path="/asthitva-teak-meadows/*" element={<Navigate replace to="/magadi-road" />} />
+      <Route path="/asthitva-cozy-corner" element={<Navigate replace to="/" />} />
+      <Route path="/asthitva-cozy-corner/*" element={<Navigate replace to="/" />} />
     </Routes>
   );
 }
